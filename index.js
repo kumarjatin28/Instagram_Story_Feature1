4d570d278964513f6d105c9ed8974c9e81e4644f
Kumar Jatin <159911170+kumarjatin28@users.noreply.github.com>
2024-03-14 21:02:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+var arr=[
+    {
+    dp:"https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story:"https://images.unsplash.com/photo-1564485377539-4af72d1f6a2f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
+
+{
+    dp:"https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story:"https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
+
+{
+    dp:"https://images.unsplash.com/photo-1604681630513-69474a4e253f?q=80&w=1527&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story:"https://images.unsplash.com/photo-1529139574466-a303027c1d8b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fG1vZGVsfGVufDB8fDB8fHww"
+},
+
+{
+    dp:"https://images.unsplash.com/photo-1512316609839-ce289d3eba0a?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story:"https://images.unsplash.com/photo-1524502397800-2eeaad7c3fe5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fG1vZGVsfGVufDB8fDB8fHww"
+},
+
+{
+    dp:"https://images.unsplash.com/photo-1590926938512-c0d7e5c39abd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fG1vZGVsfGVufDB8fDB8fHww", story:"https://images.unsplash.com/photo-1533973860717-d49dfd14cf64?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fG1vZGVsfGVufDB8fDB8fHww"
+}
+]
+
+
+var stories = document.querySelector(".stories")
+var clutter=""
+arr.forEach(function(elem,x){
+    clutter += `<div class="story">
+    <img id="${x}" src="${elem.dp}" alt="">
+</div>`
+})
+
+stories.innerHTML = clutter
+
+stories.addEventListener("click",function(dets){
+    // console.log(arr[dets.target.id].story)
+    document.querySelector(".fullscreen").style.display = "block"
+    document.querySelector(".fullscreen").style.backgroundImage = `url(${arr[dets.target.id].story})`
+    
+
+    setTimeout(function() {
+        document.querySelector(".fullscreen").style.display = "none"
+    }, 3000)
+});
